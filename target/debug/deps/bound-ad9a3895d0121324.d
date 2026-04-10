8645d4019c70050d07f2ec62fd7b0a44c608451e
elci-group <rory.spring@icloud.com>
2026-04-10 14:49:28 +0100

    kaptaind: Patch -> v0.1.2 [api-stable; paths=2; api_touches=0; deps=10; runtime=0; score=0.145; cluster=025b4782-1838-4fd0-8e80-30e168b7b125]

---
+++ target/debug/deps/bound-ad9a3895d0121324.d
@@ -0,0 +1,11 @@
+/home/adminx/bound/target/debug/deps/bound-ad9a3895d0121324.d: src/main.rs src/metadata.rs src/tree.rs src/telemetry.rs src/logging.rs src/expandable.rs src/furnace.rs
+
+/home/adminx/bound/target/debug/deps/bound-ad9a3895d0121324: src/main.rs src/metadata.rs src/tree.rs src/telemetry.rs src/logging.rs src/expandable.rs src/furnace.rs
+
+src/main.rs:
+src/metadata.rs:
+src/tree.rs:
+src/telemetry.rs:
+src/logging.rs:
+src/expandable.rs:
+src/furnace.rs:
